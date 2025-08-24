e38b2c03e87aa1ec82946f6b13c0ae61a12fa76c
alguemor <alguemor0@gmail.com>
2025-08-23 22:58:53 -0600

    main.cpp up to mcfp class specify method to use: greedy or mcfp

--- main.cpp
+++ main.cpp
@@ -1,14 +1,30 @@
 #include "problem.h"
 #include "solution.h"
 #include <iostream>
+#include <string>
+
+using namespace std;
 
 int main(int argc, char* argv[]){
+    if (argc < 4) {
+        return 1;
+    }
+    
+    string method = argv[4];
     
     Problem problema(argv[1], argv[2]);
     problema.loadData();
-
-    Solution solucion(problema);
-    solucion.solveGreedy();
-
+    
+    // eleccion metodo
+    if (method == "greedy") {
+        Greedy solver(problema);
+        solver.solveGreedy();
+    } else if (method == "mcfp") {
+        MCFP solver(problema);
+        solver.solveMCFP();
+    } else {
+        return 1;
+    }
+    
     return 0;
 }
